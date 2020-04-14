986ce3595773b9b23f12b703a3ede48a3af69d7e
Ravenor222 <ndanvers222@gmail.com>
2020-04-14 09:30:03 -0700

    server producing a product in json format on get request

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const PORT = 3001;
+const bodyParser = require("body-parser");
+
+const products = {
+    name:'skates',
+    company:'Bauer',
+    size:12,
+    id:15623,
+    picture:'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQlgWOTmrxUV6VvT_MZlNP9nne2e61h8p3RlP96JaI84zEuyuU2&usqp=CAU'
+}
+const stringProducts = JSON.stringify(products);
+
+
+app.get('/', (req, res) => {
+    res.json(products);
+})
+
+
+app.listen(PORT,() =>console.log(`Listenin' on ${PORT}`))
